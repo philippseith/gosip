5d27f474e0dd281265b406a2f8c7f05ba521ca96
Philipp <philipp.seith@gmx.net>
2023-08-25 20:09:39 +0200

    new: ReadOnlyData / Description tests

--- sip/read_test.go
+++ sip/read_test.go
@@ -22,3 +22,33 @@ func TestReadEverything(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, uint32(2), resp.DataLength)
 }
+
+func TestReadOnlyData(t *testing.T) {
+	conn, err := net.Dial("tcp", "localhost:35021")
+	defer func() { _ = conn.Close() }()
+
+	assert.NoError(t, err)
+
+	_, err = sip.Connect(conn, 3000, 10000)
+	assert.NoError(t, err)
+
+	resp, err := sip.ReadOnlyData(conn, 0, 0, 1)
+
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(2), resp.DataLength)
+}
+
+func TestReadDescription(t *testing.T) {
+	conn, err := net.Dial("tcp", "localhost:35021")
+	defer func() { _ = conn.Close() }()
+
+	assert.NoError(t, err)
+
+	_, err = sip.Connect(conn, 3000, 10000)
+	assert.NoError(t, err)
+
+	resp, err := sip.ReadDescription(conn, 0, 0, 1)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "us", resp.Unit)
+}
