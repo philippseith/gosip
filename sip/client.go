c937e30e3b641d907c643e340cd1db00aa418fef
Philipp <philipp.seith@gmx.net>
2023-12-14 19:09:06 +0100

    chg: code cleanup

--- sip/client.go
+++ sip/client.go
@@ -69,7 +69,7 @@ func WithDialBackoff(backoffFactory func() backoff.BackOff) func(c *connOptions)
 	}
 }
 
-// WithRetries configures how often a request is retried if it fails. Default is no retry.
+// WithRetries configures how often a request is retried when it fails. Default is no retry.
 func WithRetries(retries uint) func(r *requestOptions) error {
 	return func(r *requestOptions) error {
 		r.retries = retries
@@ -273,27 +273,3 @@ func doWithCancel[T any](ctx context.Context, do func() <-chan Result[T]) <-chan
 	}()
 	return ch
 }
-
-// func goWithCancel[T any](ctx context.Context, f func() (T, error)) <-chan Result[T] {
-// 	ch := make(chan Result[T], 1)
-// 	go func() {
-// 		defer close(ch)
-
-// 		select {
-// 		case <-ctx.Done():
-// 			ch <- Err[T](ctx.Err())
-// 		case r := <-func() <-chan Result[T] {
-// 			chf := make(chan Result[T])
-// 			go func() {
-// 				r := Result[T]{}
-// 				r.Ok, r.Err = f()
-// 				chf <- r
-// 				close(chf)
-// 			}()
-// 			return chf
-// 		}():
-// 			ch <- r
-// 		}
-// 	}()
-// 	return ch
-// }
