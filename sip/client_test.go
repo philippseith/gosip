ff691bd5499a722aa5a539238e7dc29bc57483e2
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2024-01-10 16:45:53 +0000

    new: stress test

--- sip/client_test.go
+++ sip/client_test.go
@@ -1,6 +1,7 @@
 package sip_test
 
 import (
+	"context"
 	"log"
 	"testing"
 	"time"
@@ -16,4 +17,32 @@ func TestClientReconnectLeaseExceeded(t *testing.T) {
 	log.Printf("Waiting %v", c.LeaseTimeout())
 	<-time.After(c.LeaseTimeout() + 500*time.Millisecond)
 	assert.NoError(t, c.Ping())
+}
+
+func TestStress(t *testing.T) {
+	c := sip.NewClient("tcp", serverAddress)
+	// the sequentialized version should be no stress at all
+	// c := sip.NewClient("tcp", serverAddress, sip.WithConcurrentTransactionLimit(1))
+	defer c.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	tick := time.NewTicker(time.Millisecond * 15)
+
+	// This loop may cause TCP ZeroWindows from the drive
+	for {
+		select {
+		case <-tick.C:
+			go func() {
+				_, err := c.ReadOnlyData(0, 0, 1)
+				if err != nil {
+					log.Printf("Error: %v", err)
+				}
+			}()
+		case <-ctx.Done():
+			return
+		}
+
+	}
 }
