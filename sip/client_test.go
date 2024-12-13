5e3a54a4620e1153df4967168d9c04af87bc81bf
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2024-12-13 16:49:59 +0100

    chg: test cork / uncork

--- sip/client_test.go
+++ sip/client_test.go
@@ -1,10 +1,9 @@
 package sip_test
 
 import (
-	"context"
+	"encoding/binary"
 	"log"
 	"sync"
-	"sync/atomic"
 	"testing"
 	"time"
 
@@ -22,43 +21,64 @@ func TestClientReconnectLeaseExceeded(t *testing.T) {
 	assert.NoError(t, c.Ping())
 }
 
-func TestStress(t *testing.T) {
-	c := sip.NewClient("tcp", serverAddress, sip.WithCorking(2*time.Millisecond))
-	// the sequential version should be no stress at all
-	// c := sip.NewClient("tcp", serverAddress, sip.WithConcurrentTransactionLimit(1))
+func TestBackupNoCork(t *testing.T) {
+	c := sip.NewClient("tcp", serverAddress)
+
 	var wg sync.WaitGroup
 	defer func() {
-		wg.Wait()
 		_ = c.Close()
 	}()
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-
-	tick := time.NewTicker(time.Millisecond * 5)
-	count := int32(0)
+	s192, _ := c.ReadOnlyData(0, 0, 192)
+	idns := make([]uint32, len(s192.Data)/4)
+	for i := 0; i < len(idns); i++ {
+		idns[i] = binary.LittleEndian.Uint32(s192.Data[i*4 : (i+1)*4])
+	}
 
 	// This loop may cause TCP ZeroWindows from the drive
-	for {
-		select {
-		case <-tick.C:
-			go func() {
-				select {
-				case <-ctx.Done():
-					return
-				default:
-					wg.Add(1)
-					_, err := c.ReadOnlyData(0, 0, 1)
-					if err != nil {
-						log.Printf("Error: %v", err)
-					}
-					wg.Done()
-					atomic.AddInt32(&count, 1)
-				}
-			}()
-		case <-ctx.Done():
-			log.Printf("count: %v", count)
-			return
-		}
+	for _, idn := range idns {
+		cIdn := idn
+		wg.Add(1)
+		go func() {
+			// log.Printf("%s", sip.Idn(cIdn))
+			_, err := c.ReadEverything(0, 0, cIdn)
+			if err != nil {
+				log.Printf("Error: %v", err)
+			}
+			//log.Printf("RECV %s", sip.Idn(cIdn))
+			wg.Done()
+		}()
 	}
+	wg.Wait()
+}
+
+func TestBackupCork(t *testing.T) {
+	c := sip.NewClient("tcp", serverAddress, sip.WithCorking(2*time.Millisecond))
+
+	var wg sync.WaitGroup
+	defer func() {
+		_ = c.Close()
+	}()
+
+	s192, _ := c.ReadOnlyData(0, 0, 192)
+	idns := make([]uint32, len(s192.Data)/4)
+	for i := 0; i < len(idns); i++ {
+		idns[i] = binary.LittleEndian.Uint32(s192.Data[i*4 : (i+1)*4])
+	}
+
+	// This loop may cause TCP ZeroWindows from the drive
+	for _, idn := range idns {
+		cIdn := idn
+		wg.Add(1)
+		go func() {
+			// log.Printf("%s", sip.Idn(cIdn))
+			_, err := c.ReadEverything(0, 0, cIdn)
+			if err != nil {
+				log.Printf("Error: %v", err)
+			}
+			//log.Printf("RECV %s", sip.Idn(cIdn))
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
